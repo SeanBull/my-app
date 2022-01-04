83191bb57690f082642528bbeadfddef6a66be9f
seandbull <sean.bull@rbs.co.uk>
2022-01-04 11:38:40 +0000

    fixed smoke grep

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello World, my name is Sean :)</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SeanBull"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github
         </a>
       </header>
     </div>
